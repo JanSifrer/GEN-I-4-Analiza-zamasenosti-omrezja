eb4d38bbad219697e4e02a4e1d344b8756112149
Jan Šifrer <jan.sifrer1@gmail.com>
2022-05-10 14:25:48 +0200

    malo popravil kodo.

--- program.R
+++ program.R
@@ -24,8 +24,9 @@ X1_ptdf_ram$Name <- str_replace_all(X1_ptdf_ram$Name,"D8", "DE")
 
 #Change the names
 X1_ptdf_ram[c("Name1", "Name3")] <- str_split_fixed(X1_ptdf_ram$Name, " ", 2)
-X1_ptdf_ram[c("Name2", "Name4")] <- str_split_fixed(X1_ptdf_ram$Name3, "_LTA",2)
-X1_ptdf_ram <- X1_ptdf_ram[,c(-15,-17)]
+X1_ptdf_ram[c("Name4", "Name5")] <- str_split_fixed(X1_ptdf_ram$Name3, "_LTA",2)
+X1_ptdf_ram[c("Name2", "Name6")] <- str_split_fixed(X1_ptdf_ram$Name4, "] - ",2)
+X1_ptdf_ram <- X1_ptdf_ram[,c(-15,-16,-17,-19)]
 
 #part of Swiss 
 #ch <- grepl("CH", X1_ptdf_ram$Name1, fixed = TRUE)
@@ -94,105 +95,147 @@ testna <- testna[testna$what < 700,]
 #koef <- testna[testna$RAM == 2952,]
 koef <- testna
 #koef <- calculate_coefficients(Joined,time_start,time_end,day_start,day_end)
-test <- lm(c ~ AT + BE + DE + FR + NL + ALBE + ALDE-1, koef)
-test1 <- lm(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM-1, koef)
-test2 <- lm(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM, koef)
-test5 <- lm(what ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM-1, koef)
-
-summary(test)$adj.r.squared
-summary(test1)$adj.r.squared
-summary(test2)$adj.r.squared
-summary(test5)$adj.r.squared
-
-#plot(density(koef$what))
+#test <- lm(c ~ AT + BE + DE + FR + NL + ALBE + ALDE-1, koef)
+#test1 <- lm(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM-1, koef)
+#test2 <- lm(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM, koef)
+#test5 <- lm(what ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM-1, koef)
 #
-library(lme4)
+#summary(test)$adj.r.squared
+#summary(test1)$adj.r.squared
+#summary(test2)$adj.r.squared
+#summary(test5)$adj.r.squared
+#
+##plot(density(koef$what))
+##
+#library(lme4)
 koef[koef$Name2 == "",]$Name2 = koef[koef$Name2 == "",]$Name1
-
-hh <- lmer(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM + (1|Name1/Day) + (1|Name1/Time), data = koef)
-hh1 <- lmer(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM + (1|Day) + (1|Time) + (1|Name1), data = koef)
-hh2 <- lmer(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM + (1|Name2/Day) + (1|Name2/Time), data = koef)
-hh3 <- lmer(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM + (1|Day) + (1|Time) + (1|Name2), data = koef)
-
-
-#hh2per <- performance::check_predictions(hh2) #good
-#performance::check_predictions(hh1) #good
-#performance::check_predictions(hh)  #also good, but no so good as hh2
 #
-#performance::check_predictions(test)  #not good
-#performance::check_predictions(test1) #not too bad
-#performance::check_predictions(test2) #not bad
-#performance::check_predictions(test5) #bad
+#hh <- lmer(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM + (1|Name1/Day) + (1|Name1/Time), data = koef)
+#hh1 <- lmer(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM + (1|Day) + (1|Time) + (1|Name1), data = koef)
+#hh2 <- lmer(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM + (1|Name2/Day) + (1|Name2/Time), data = koef)
+#hh3 <- lmer(c ~ AT + BE + DE + FR + NL + ALBE + ALDE + RAM + (1|Day) + (1|Time) + (1|Name2), data = koef)
 #
-#performance::performance_accuracy(hh2)
-#a <- performance::compare_performance(hh2,hh1,hh,test,test1,test2)
-#from this test we see, that the hh2 model is the best.
-
-#ranef(hh)
-#ranef(hh)$'Time:Name1'["00:00:00:[DE-NL]",]
-#ranef(hh)$'Day:Name1'["1:[DE-NL]",]
-#ranef(hh)$Name1["[DE-NL]",]
-
-
-#Test predictions:
-
-#true value is: 17.02078
-#c = 1381.979
-PTDF <- data.frame("Day" = 1,"Name2" = "Diele - Meeden 380 Black [DIR] [DE]", "Name1" = "[DE-NL]", "Time" = "00:00:00","AT"=0.58860, "BE"=0.25637, "DE"=0.58860, "FR"=0.38150, "NL"=0, "ALBE"=-0.30955, "ALDE"=0, "RAM"=1399)
-#day <- 1
-#time <- 00:00:00
-#name <- [DE-NL]
-predict(hh, PTDF)
-predict(hh1, PTDF)
-predict(hh2, PTDF)
-
-predict.lm(test2,PTDF,interval = c("prediction"))
-
-#true value is: -0.27015
-#c = 1075.2701
-PTDF <- data.frame("Day" = 2,"Name2"="Ensdorf - Vigy 2S [OPP] [DE]", "Name1" = "[DE-FR]", "Time" = "02:00:00","AT"=0.02458, "BE"=0.13275, "DE"=-0.01193, "FR"=0.20635, "NL"=0.02558, "ALBE"=0.10677, "ALDE"=-0.06474, "RAM" =1075)
-#day <- 2
-#time <- 02:00:00
-#name <- [DE-FR]
-predict(hh, PTDF)
-predict(hh1, PTDF)
-predict(hh2,PTDF)
-predict.lm(test2,PTDF,interval = c("prediction"))
-
-#true value is: 0.14847
-#c = 2951.852
-PTDF <- data.frame("Day" = 1, "Name2" = "Pleinting - St. Peter 258 [DIR] [AT]","Name1" = "[DE-AT]", "Time" = "17:00:00","AT"=0.15915, "BE"=0, "DE"=0.15915, "FR"=0.97386, "NL"=0, "ALBE"=0.03069, "ALDE"=0, "RAM" =2952)
-#day <- 1
-#time <- 17:00:00
-#name <- [DE-AT]
-predict(hh, PTDF)
-predict(hh1, PTDF)
-predict(hh2,PTDF)
-predict.lm(test2,PTDF,interval = c("prediction"))
-
-#true value is: 3.76918
-#c = 7978.231
-PTDF <- data.frame("Day" = 7, "Name2"="Buers Transformer 37 [DIR]" ,"Name1" = "[DE-DE]", "Time" = "11:00:00","AT"=-0.41615, "BE"=0, "DE"=0.79506, "FR"=0, "NL"=0, "ALBE"=0.44125, "ALDE"=0, "RAM"=7982)
-#day <- 7
-#time <- 11:00:00
-#name <- [DE-DE]
-predict(hh, PTDF)
-predict(hh1, PTDF)
-predict(hh2, PTDF)
-predict.lm(test2,PTDF,interval = c("prediction"))
-
-
-#true value is: 353.46941
-#c = 7628.531
-PTDF <- data.frame("Day" = 4,"Name2"="Diele - Meeden 380 Black [DIR] [DE]", "Name1" = "[DE-NL]", "Time" = "22:00:00","AT"=-0.31925, "BE"=0.0645, "DE"=0.90467, "FR"=0.0645, "NL"=0, "ALBE"=-0.26706, "ALDE"=0, "RAM"=7982)
-#day <- 4
-#time <- 22:00:00
-#name <- [DE-NL]
-predict(hh, PTDF)
-predict(hh1, PTDF)
-predict(hh2, PTDF)
-predict.lm(test2,PTDF,interval = c("prediction"))
-
-#ranef(hh1)
-#ranef(hh1)$'Time:Day'["00:00:00:1",]
-#ranef(hh1)$Day["1",]
+#
+#
+######IF )# LINE OF THE CODE IS SET TO TOO LITLE; THAN THIS DON'T WORK!
+##hh2per <- performance::check_predictions(hh2) #good
+##performance::check_predictions(hh1) #good
+##performance::check_predictions(hh)  #also good, but no so good as hh2
+##
+##performance::check_predictions(test)  #not good
+##performance::check_predictions(test1) #not too bad
+##performance::check_predictions(test2) #not bad
+##performance::check_predictions(test5) #bad
+##
+##performance::performance_accuracy(hh2)
+##a <- performance::compare_performance(hh2,hh1,hh,test,test1,test2)
+##from this test we see, that the hh2 model is the best.
+#
+##ranef(hh)
+##ranef(hh)$'Time:Name1'["00:00:00:[DE-NL]",]
+##ranef(hh)$'Day:Name1'["1:[DE-NL]",]
+##ranef(hh)$Name1["[DE-NL]",]
+##Test predictions:
+#
+#hh2.res <- summary(hh2)$residuals
+#sigma.my <- sigma(hh2)
+#
+write.csv(koef,"koef.csv", row.names = FALSE)
+#
+##true value is: 17.02078
+##c = 1381.979
+#PTDF <- data.frame("Day" = 1,"Name2" = "Diele - Meeden 380 Black [DIR] [DE]", "Name1" = "[DE-NL]", "Time" = "00:00:00","AT"=0.58860, "BE"=0.25637, "DE"=0.58860, "FR"=0.38150, "NL"=0, "ALBE"=-0.30955, "ALDE"=0, "RAM"=1399)
+##day <- 1
+##time <- 00:00:00
+##name <- [DE-NL]
+#RAM=1399
+#predict(hh, PTDF)
+#predict(hh1, PTDF)
+#predict(hh3, PTDF)
+#napoved <- predict(hh2,PTDF)
+#interval <- (c(napoved - 1.96*sigma.my, napoved + 1.96*sigma.my))
+#napoved
+#interval
+#probability.of.congestion <- 1-(interval[2]-RAM)/(interval[2]-interval[1])
+#probability.of.congestion
+#
+#predict.lm(test2,PTDF,interval = c("prediction"))
+#
+##true value is: -0.27015
+##c = 1075.2701
+#PTDF <- data.frame("Day" = 2,"Name2"="Ensdorf - Vigy 2S [OPP] [DE]", "Name1" = "[DE-FR]", "Time" = "02:00:00","AT"=0.02458, "BE"=0.13275, "DE"=-0.01193, "FR"=0.20635, "NL"=0.02558, "ALBE"=0.10677, "ALDE"=-0.06474, "RAM" =1075)
+##day <- 2
+##time <- 02:00:00
+##name <- [DE-FR]
+#RAM = 1075
+#predict(hh, PTDF)
+#predict(hh1, PTDF)
+#napoved <- predict(hh2,PTDF)
+#interval <- (c(napoved - 1.96*sigma.my, napoved + 1.96*sigma.my))
+#napoved
+#interval
+#probability.of.congestion <- (RAM-interval[1])/(interval[2]-interval[1])*0.95
+#probability.of.congestion
+#
+#predict.lm(test2,PTDF,interval = c("prediction"))
+#
+##true value is: 0.14847
+##c = 2951.852
+#PTDF <- data.frame("Day" = 1, "Name2" = "Pleinting - St. Peter 258 [DIR] [AT]","Name1" = "[DE-AT]", "Time" = "17:00:00","AT"=0.15915, "BE"=0, "DE"=0.15915, "FR"=0.97386, "NL"=0, "ALBE"=0.03069, "ALDE"=0, "RAM" =2952)
+##day <- 1
+##time <- 17:00:00
+##name <- [DE-AT]
+#RAM =2952
+#predict(hh, PTDF)
+#predict(hh1, PTDF)
+#napoved <- predict(hh2,PTDF)
+#interval <- (c(napoved - 1.96*sigma.my, napoved + 1.96*sigma.my))
+#napoved
+#interval
+#probability.of.congestion <- (RAM-interval[1])/(interval[2]-interval[1])*0.95
+#probability.of.congestion
+#
+#predict.lm(test2,PTDF,interval = c("prediction"))
+#
+##true value is: 3.76918
+##c = 7978.231
+#PTDF <- data.frame("Day" = 7, "Name2"="Buers Transformer 37 [DIR]" ,"Name1" = "[DE-DE]", "Time" = "11:00:00","AT"=-0.41615, "BE"=0, "DE"=0.79506, "FR"=0, "NL"=0, "ALBE"=0.44125, "ALDE"=0, "RAM"=7982)
+##day <- 7
+##time <- 11:00:00
+##name <- [DE-DE]
+#RAM=7982
+#predict(hh, PTDF)
+#predict(hh1, PTDF)
+#napoved <- predict(hh2,PTDF)
+#interval <- (c(napoved - 1.96*sigma.my, napoved + 1.96*sigma.my))
+#napoved
+#interval
+#probability.of.congestion <- (RAM-interval[1])/(interval[2]-interval[1])*0.95
+#probability.of.congestion
+#
+#predict.lm(test2,PTDF,interval = c("prediction"))
+#
+#
+##true value is: 353.46941
+##c = 7628.531
+#PTDF <- data.frame("Day" = 4,"Name2"="Diele - Meeden 380 Black [DIR] [DE]", "Name1" = "[DE-NL]", "Time" = "22:00:00","AT"=-0.31925, "BE"=0.0645, "DE"=0.90467, "FR"=0.0645, "NL"=0, "ALBE"=-0.26706, "ALDE"=0, "RAM"=7982)
+##day <- 4
+##time <- 22:00:00
+##name <- [DE-NL]
+#RAM=7982
+#predict(hh, PTDF)
+#predict(hh1, PTDF)
+#napoved <- predict(hh2,PTDF)
+#interval <- (c(napoved - 1.96*sigma.my, napoved + 1.96*sigma.my))
+#napoved
+#interval
+#probability.of.congestion <- (RAM-interval[1])/(interval[2]-interval[1])*0.95
+#probability.of.congestion
+#
+#predict.lm(test2,PTDF,interval = c("prediction"))
+#
+##ranef(hh1)
+##ranef(hh1)$'Time:Day'["00:00:00:1",]
+##ranef(hh1)$Day["1",]
+#
+# 
